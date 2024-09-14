2aaf6ee0b9888cf5401d72fc8a1aeb2be550716e
mananatal <mananatal25@gmail.com>
2024-09-15 01:15:47 +0530

    Add Auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+import { User } from "../models/User.model";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+
+
+const verifyJWT=asyncHandler(async (req,res)=>{
+    try {
+        const accesstoken=req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ","");
+    
+        if(!accesstoken){
+            throw new ApiError(404,"Access Token not found");
+        }
+    
+        const decodedToken=jwt.verify(accesstoken,process.env.ACCESS_TOKEN_SECRET);
+    
+        const user=await User.findById(decodedToken?._id).select("-password -refreshToken");
+    
+        req.user=user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token");
+    }
+})
